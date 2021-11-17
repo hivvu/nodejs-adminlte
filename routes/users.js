48bc269729cd150fa0a6e6016ecbabeb705020d4
Ivo Pereira <ivo.pereira@wingman.pt>
2021-11-17 23:25:30 +0000

    List login users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var authentication_mdl = require('../middleware/authentication');
+var session_store;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authentication_mdl.is_login, function (req, res, next) {
+    req.getConnection(function (err, connection) {
+        var query = connection.query('SELECT * FROM user', function (err, rows) {
+            if (err)
+                var error = ("Error Selecting : %s ", err);
+
+            req.flash('msg_error', error);
+            res.render('users/list', { title: "Users", data: rows, session_store: req.session});
+        });
+    });
 });
 
 module.exports = router;
